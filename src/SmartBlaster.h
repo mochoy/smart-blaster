8e4510b557e8bfa5985cd962c8c76a5a98983e0d
BobLakensi <montgomery.okdaily@gmail.com>
2017-09-02 22:02:15 -0700

    import new libs

--- src/SmartBlaster.h
+++ src/SmartBlaster.h
@@ -3,8 +3,15 @@
 
 #include "Arduino.h"
 
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
 #include <Button.h>
 
 #include <StandardCplusplus.h>
